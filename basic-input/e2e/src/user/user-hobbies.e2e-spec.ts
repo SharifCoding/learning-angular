ef32241c80a5dc4859918017df53203738cee81c
SharifCoding <ocarinaoftime81@gmail.com>
2018-12-27 10:32:15 +0000

    basic-input e2e hobby testing

--- basic-input/e2e/src/user/user-hobbies.e2e-spec.ts
+++ basic-input/e2e/src/user/user-hobbies.e2e-spec.ts
@@ -2,6 +2,8 @@ import { AppPage } from './user-hobbies.po';
 
 describe('User hobbies check on Basic Input App', () => {
   let page: AppPage;
+  let insertHobby = 'Beer';
+  let hobbyNumber = 0;
 
   beforeEach(() => {
     page = new AppPage();
@@ -10,5 +12,39 @@ describe('User hobbies check on Basic Input App', () => {
 
   it('should display the h1 hobbies text', () => {
     expect(page.getLastParagraphText()).toEqual('Hobbies');
+  });
+
+  it('should display default hobby list', () => {
+    page.getHobbyList().then((items) => {
+      expect(items.length).toBe(3);
+      expect(items[0].getText()).toContain('Donuts');
+      expect(items[1].getText()).toContain('More');
+      expect(items[2].getText()).toContain('Everything');
+    });
+  });
+
+  it('insert new hobby to current list', () => {
+    page.inputNewHobby(insertHobby);
+    page.getHobbyList().then((items) => {
+      expect(items[0].getText()).toContain(insertHobby);
+    });
+  });
+
+  it('remove a hobby from the hobby list', () => {
+    page.removeFirstHobby(hobbyNumber);
+    page.getHobbyList().then((items) => {
+      expect(items[hobbyNumber].getText()).toContain('More');
+    });
+  });
+
+  it('insert and remove a hobby from the list', () => {
+    page.inputNewHobby(insertHobby);
+    page.getHobbyList().then((items) => {
+      expect(items[0].getText()).toContain(insertHobby);
+    });
+    page.removeFirstHobby(hobbyNumber + 1);
+    page.getHobbyList().then((items) => {
+      expect(items[0].getText()).toContain(insertHobby);
+    });
   });
 });
--- basic-input/e2e/src/user/user-hobbies.po.ts
+++ basic-input/e2e/src/user/user-hobbies.po.ts
@@ -1,11 +1,28 @@
 import { browser, by, element } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/')
   }
 
   getLastParagraphText() {
-    return element.all(by.css('app-user h1')).last().getText();
+    return element.all(by.css('app-user h1')).last().getText()
+  }
+
+  getHobbyList() {
+    return element.all(by.css('.hobbies li'))
+  }
+
+  inputNewHobby(newHobby) {
+    let queryInput = element(by.css('[name="hobby"]'))
+    return queryInput.clear().then(() => {
+      queryInput.sendKeys(newHobby)
+      browser.actions().sendKeys(protractor.Key.ENTER).perform()
+    })
+  }
+
+  removeFirstHobby(hobbyNumber) {
+    return element.all(by.buttonText('X')).get(hobbyNumber).click();
   }
 }
