0d786e539ee867a005c33edafd71f58b34ff093a
SharifCoding <ocarinaoftime81@gmail.com>
2018-12-28 14:27:27 +0000

    contact-list e2e customer-details test

--- contact-list/e2e/src/customer-details/customer-details.e2e-spec.ts
+++ contact-list/e2e/src/customer-details/customer-details.e2e-spec.ts
@@ -3,15 +3,38 @@ import { browser } from 'protractor';
 
 describe('customer-details check on the contact-list app', () => {
   let page: AppPage;
-  let id = 1; // 1 to 5 card displays
+  let id: number;
 
   beforeEach(() => {
     page = new AppPage();
+    id = Math.floor(Math.random() * (5 - 1) + 1);
+
     page.navigateTo();
+    page.clickCustomerDetails(id);
   });
 
   it('use correct URL for customer details', () => {
-    page.firstCustomerDetails(id);
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/customerdetails/' + id);
+  });
+
+  it('should display h1 welcome message', () => {
+    expect(page.getWelcomeText()).toEqual('Welcome to contact-list!');
+  });
+
+  it('should display customer details heading', () => {
+    expect(page.getHeadingText()).toEqual('Customer Details');
+  });
+
+  it('should display customer name', () => {
+    expect(page.checkCustomerName()).toContain('Simpson');
+  });
+
+  it('showing all customer information', () => {
+    expect(page.getCustomerDetails().count()).toBe(13);
+  });
+
+  it('press Go Back button and return to base URL', () => {
+    expect(page.pressGoBackButton()).toBeTruthy;
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/customers');
   });
 });
--- contact-list/e2e/src/customer-details/customer-details.po.ts
+++ contact-list/e2e/src/customer-details/customer-details.po.ts
@@ -5,7 +5,27 @@ export class AppPage {
     return browser.get('/');
   }
 
-  firstCustomerDetails(getId: number) {
+  clickCustomerDetails(getId: number) {
     return element.all(by.className('btn btn-primary')).get(getId - 1).click();
+  }
+
+  getWelcomeText() {
+    return element(by.css('app-root h1')).getText();
+  }
+
+  getHeadingText() {
+    return element(by.className('card-header')).getText();
+  }
+
+  checkCustomerName() {
+    return element(by.css('.card-title')).getText();
+  }
+
+  getCustomerDetails() {
+    return element.all(by.className('card-text'));
+  }
+
+  pressGoBackButton() {
+    return element(by.cssContainingText('.btn', 'Go Back')).click();
   }
 }
